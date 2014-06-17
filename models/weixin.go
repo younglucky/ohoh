b576c314dfa650bf6343a3dd992a5c9bc8f5a64f
younglucky <younglucky@aliyun.com>
2014-06-17 15:27:15 +0800

    add test save data to file

--- models/weixin.go
+++ models/weixin.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"encoding/json"
 	"github.com/wendal/goweixin"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 type WeixinHandler struct {
@@ -24,6 +27,7 @@ func (w *WeixinHandler) Link(msg goweixin.Message) goweixin.Replay {
 func (w *WeixinHandler) Event(msg goweixin.Message) goweixin.Replay {
 	log.Println("event:", msg)
 	if msg.Event() == "subscribe" {
+		SaveUser(msg)
 		return goweixin.ReplyText("欢迎关注，hello world!")
 	}
 	return w.Default(msg)
@@ -33,4 +37,23 @@ func (w *WeixinHandler) Voice(msg goweixin.Message) goweixin.Replay {
 }
 func (w *WeixinHandler) Default(msg goweixin.Message) goweixin.Replay {
 	return nil
+}
+
+func SaveUser(msg goweixin.Message) {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	fileName := "./data/user.json"
+	err = ioutil.WriteFile(filename, bytes, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+	msg := goweixin.Message{"hello": "world"}
+	SaveUser(msg)
 }
