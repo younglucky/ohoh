ca1bb707dadc0f603561187169f7c033427a1039
younglucky <younglucky@aliyun.com>
2014-06-17 15:26:23 +0800

    add test save data to file

--- models/weixin.go
+++ models/weixin.go
@@ -9,7 +9,28 @@ type WeixinHandler struct {
 	*goweixin.BaseWeiXinHandler
 }
 
-func (w *WeixinHandler) Event(msg goweixin.Message) (reply goweixin.Replay) {
-	log.Println("event:", msg.Event())
-	return goweixin.ReplyText("欢迎关注，hello world!")
+func (w *WeixinHandler) Text(msg goweixin.Message) goweixin.Replay {
+	return h.Default(msg)
+}
+func (w *WeixinHandler) Image(msg goweixin.Message) goweixin.Replay {
+	return h.Default(msg)
+}
+func (w *WeixinHandler) Location(msg goweixin.Message) goweixin.Replay {
+	return h.Default(msg)
+}
+func (w *WeixinHandler) Link(msg goweixin.Message) goweixin.Replay {
+	return h.Default(msg)
+}
+func (w *WeixinHandler) Event(msg goweixin.Message) goweixin.Replay {
+	log.Println("event:", msg)
+	if msg.Event() == "subscribe" {
+		return goweixin.ReplyText("欢迎关注，hello world!")
+	}
+	return w.Default(msg)
+}
+func (w *WeixinHandler) Voice(msg goweixin.Message) goweixin.Replay {
+	return h.Default(msg)
+}
+func (w *WeixinHandler) Default(msg goweixin.Message) goweixin.Replay {
+	return nil
 }
